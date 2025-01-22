30e2fc2bb64dd25b9ea15a32da62808ea5a51e17
tmdgusya <dev0jsh@gmail.com>
2025-01-22 21:42:50 +0900

    first commit

---
+++ model/build.gradle.kts
@@ -0,0 +1,11 @@
+plugins {
+    // Apply the shared build logic from a convention plugin.
+    // The shared code is located in `buildSrc/src/main/kotlin/kotlin-jvm.gradle.kts`.
+    id("buildsrc.convention.kotlin-jvm")
+    // Apply Kotlin Serialization plugin from `gradle/libs.versions.toml`.
+    alias(libs.plugins.kotlinPluginSerialization)
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
---
+++ model/src/main/kotlin/Model.kt
@@ -0,0 +1,10 @@
+/**
+ *
+ * This interface for model to call and
+ *
+ * @author roach<dev0jsh@gmail.com>
+ */
+interface Model {
+
+    fun call()
+}
